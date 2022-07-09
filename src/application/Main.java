0cb29ac5a8a7efb9d925c0d2cd3a06ea91c29066
graciele.oliveira <oliveiragracie@gmail.com>
2022-07-09 14:25:49 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;  //expor referencia para a cena
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true); 
 			scrollPane.setFitToWidth(true);	
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -27,7 +30,13 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	//metodo para pegar a cena
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -33,16 +42,43 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
 	
 	
 	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
-		
+	public void initialize(URL uri, ResourceBundle rb) {	
 		
 	}
+	
+	//função para trabalhar com outra tela
+	//synchronized = garante que a execução não será interropmpida visto que a aplicação trabalha com várias threds sendo carregadas
+	
+	private synchronized void loadView(String absoluteName) {   					//absolute pq vai passar o caminho completo
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));  
+			VBox newVBox = loader.load();	
+			
+			//mostrar aboutview dentro da view principal
+			//1 - pegar referencia da cena
+			Scene mainScene = Main.getMainScene();
+			
+			//2- pegar referencia para o VBox da janela principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0); 			//1º filho de VBox
+			mainVBox.getChildren().clear(); 						//limpa todo o menu
+			mainVBox.getChildren().add(mainMenu);	
+			mainVBox.getChildren().addAll(newVBox.getChildren());	//add o menu e filhos do About.fxml
+			
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+	
 	
 
 }
