c193fd632c349ad3bc478eb5261355932221f658
graciele.oliveira <oliveiragracie@gmail.com>
2022-07-11 18:14:38 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -8,7 +8,7 @@ public class Department implements Serializable {
 
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
@@ -33,7 +33,6 @@ public class Department implements Serializable {
 		this.name = name;
 	}
 
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -41,7 +40,6 @@ public class Department implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -60,7 +58,6 @@ public class Department implements Serializable {
 		return true;
 	}
 
-	
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
--- src/model/service/DepartmentService.java
+++ src/model/service/DepartmentService.java
@@ -1,17 +1,17 @@
 package model.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
-	public List<Department> findAll(){
-		List<Department> lst = new ArrayList<>();
-		lst.add(new Department(1, "Um"));
-		lst.add(new Department(2, "Dois"));
-		return lst;
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public List<Department> findAll(){	
+		return dao.findAll();
 	}
 
 }
